afd08141967c61fc30680268a363eede7d366aa6
rutikkatoriwala <22bmiit112@gmail.com>
2026-01-31 20:49:54 +0530

    Add Render deployment configuration

--- app.py
+++ app.py
@@ -303,4 +303,6 @@ def url_scan():
         
         
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
